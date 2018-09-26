80c7d30528ba6553b49a66d7d79de4682d57975e
sho.endo <s.endo.8951@gmail.com>
2018-09-26 08:26:04 +0000

    modify login function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
 
   def require_user_logged_in
     unless logged_in?
+      flash[:info] = "ログインが必要です"
       redirect_to login_url
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "ユーザーを新規作成しました"
       redirect_to @user
     else
